ead595c23db2d38e7dd5206ec483e28e20ac12c1
Sushree Sangita Pani <sushree@anterntech.com>
2024-03-13 15:04:08 +0530

    Enquiry Model Creation

---
+++ StreamlineAcademy.Api/StreamlineAcademy.Application/RRModels/EnquiryRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamlineAcademy.Application.RRModels
+{
+    public class EnquiryRequest // Clint request model
+    {
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+    }
+
+    public class EnquiryResponse:EnquiryRequest // to send response to clint
+    {
+        public Guid Id { get; set; }
+        
+    }
+
+    public class EnquiryUpdateRequest:EnquiryResponse  // to update  response 
+    {
+
+    }
+}
---
+++ StreamlineAcademy.Api/StreamlineAcademy.Domain/Entities/Enquiry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamlineAcademy.Domain.Entities
+{
+    public class Enquiry
+    {
+        [Required (ErrorMessage ="Name is required")]
+        [StringLength (40 ,ErrorMessage= "Name must not exceed 50 characters.")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage ="Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Phone number is required.")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone number must be 10 digits.")]
+        public string PhoneNumber { get; set; } = null!;
+
+    }
+}
