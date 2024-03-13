4c6d2a5e483df5716a56b2466b98db1d56c7f929
AAMIR BASHIR <112857472+amykash80@users.noreply.github.com>
2024-03-13 14:24:34 +0530

    Entity Framework Core (sql,design ,tools) installed along with Automapper

---
+++ StreamlineAcademy.Api/StreamlineAcademy.Application/DI/AssemblyRefrencecs.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamlineAcademy.Application.DI
+{
+    public static class AssemblyRefrencecs
+    {
+
+        public static IServiceCollection AddAplicationService(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
