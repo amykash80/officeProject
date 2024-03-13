f0b1ad65705a133e117514555bf8aae88f22de4a
AAMIR BASHIR <112857472+amykash80@users.noreply.github.com>
2024-03-13 11:06:34 +0530

    demo

--- StreamlineAcademy.Api/StreamlineAcademy.Api/Program.cs
+++ StreamlineAcademy.Api/StreamlineAcademy.Api/Program.cs
@@ -8,7 +8,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-// demo changes
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
