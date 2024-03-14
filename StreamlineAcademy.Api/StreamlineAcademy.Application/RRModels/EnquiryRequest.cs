cfbff35ea89e0c5f83afb4d71b2a8a1e1adfd3c2
AAMIR BASHIR <112857472+amykash80@users.noreply.github.com>
2024-03-14 12:24:56 +0530

    Generic Repository Created

--- StreamlineAcademy.Api/StreamlineAcademy.Application/RRModels/EnquiryRequest.cs
+++ StreamlineAcademy.Api/StreamlineAcademy.Application/RRModels/EnquiryRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,19 @@ namespace StreamlineAcademy.Application.RRModels
 {
     public class EnquiryRequest // Clint request model
     {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(40, ErrorMessage = "Name must not exceed 50 characters.")]
         public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        [RegularExpression("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", ErrorMessage = "Please Enter Valid Email")]
         public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Phone number is required.")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone number must be 10 digits.")]
         public string PhoneNumber { get; set; } = null!;
+
     }
 
     public class EnquiryResponse:EnquiryRequest // to send response to clint
--- StreamlineAcademy.Api/StreamlineAcademy.Domain/Entities/Enquiry.cs
+++ StreamlineAcademy.Api/StreamlineAcademy.Domain/Entities/Enquiry.cs
@@ -10,16 +10,9 @@ namespace StreamlineAcademy.Domain.Entities
 {
     public class Enquiry:BaseModel
     {
-        [Required (ErrorMessage ="Name is required")]
-        [StringLength (40 ,ErrorMessage= "Name must not exceed 50 characters.")]
+    
         public string Name { get; set; } = null!;
-
-        [Required(ErrorMessage ="Email is required")]
-        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string Email { get; set; } = null!;
-
-        [Required(ErrorMessage = "Phone number is required.")]
-        [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone number must be 10 digits.")]
         public string PhoneNumber { get; set; } = null!;
 
     }
