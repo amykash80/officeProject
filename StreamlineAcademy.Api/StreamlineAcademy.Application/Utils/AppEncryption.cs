44a3de68d84e34394b9bd334f8fc9e31475e5cd5
AAMIR BASHIR <112857472+amykash80@users.noreply.github.com>
2024-03-28 11:03:36 +0530

    update AppEncryption.cs

--- StreamlineAcademy.Api/StreamlineAcademy.Application/Utils/AppEncryption.cs
+++ StreamlineAcademy.Api/StreamlineAcademy.Application/Utils/AppEncryption.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,15 +9,21 @@ namespace StreamlineAcademy.Application.Utils
 {
     public class AppEncryption
     {
-        public static string HashPassword(string password, string salt)
-        {
-            return BCrypt.Net.BCrypt.HashPassword(password, salt);
-
-        }
-
-        public static string GenerateSalt()
-        {
-            return BCrypt.Net.BCrypt.GenerateSalt();
-        }
-    }
+		public static string GenerateSalt()
+		{
+			var randomNum = RandomNumberGenerator.GetBytes(16);
+			return Convert.ToBase64String(randomNum);
+		}
+	     public static string CreatePassword(string password, string salt)
+		{
+			var PasswordWithSalt = string.Concat(password, salt);
+			var sha = SHA256.Create();
+			var bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(PassWithSalt!));
+			return Convert.ToBase64String(bytes);
+		}
+		public static bool ComparePassword(string hashPassword, string password, string salt)
+		{
+			return hashPassword == CreatePassword(password, salt);
+		}
+	}
 }
